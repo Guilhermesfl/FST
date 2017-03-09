cb22da2c54676e99a4bdfa6989ec8b34e68c5637
Guilhermesfl <gsfl3101@gmail.com>
2017-03-09 14:40:58 -0300

    Insert function tested for prefixes 1 and 0 with stride size equal to 1

--- FST.c
+++ FST.c
@@ -12,7 +12,10 @@ FSTnode* NewNode(int stride_size){
 	FSTnode* x = (FSTnode*)malloc(sizeof(FSTnode));
   	x->entries = (entry *)malloc(n_entries*sizeof(entry));
 
-  	for (int i = 0; i < n_entries; ++i) x->entries[i].child = NULL;
+  	for (int i = 0; i < n_entries; ++i){
+		x->entries[i].child = NULL;
+  		for (int j = 0; j < 32; ++j) x->entries[i].next_hop[j] = -1;
+  	}
 
 	return x; 
 }
@@ -20,8 +23,8 @@ FSTnode* NewNode(int stride_size){
 FSTnode* insert(FSTnode* node,int pfx[], int stride_size, int *pos_pfx, int pfx_size) {
 	
 	int pos = convert_bin(pfx,stride_size,pos_pfx,pfx_size);
-	if (*pos_pfx == (pfx_size-1)){ //If true, end of pfx
-		for (int i = 0; i < pfx_size-1; ++i)
+	if (*pos_pfx == pfx_size){ //If true, end of pfx
+		for (int i = 0; i < pfx_size; ++i)
 		{
 			node->entries[pos].next_hop[i] = pfx[i];
 		}
@@ -38,17 +41,17 @@ FSTnode* insert(FSTnode* node,int pfx[], int stride_size, int *pos_pfx, int pfx_
 
 int convert_bin(int pfx[], int stride_size, int *pos_pfx, int pfx_size){
 
-	int pos = 0,aux = stride_size-1, aux1=0;
-	for (int i = *pos_pfx; i < (stride_size+*pos_pfx); ++i)
+	int pos = 0,aux=stride_size-1,aux1,aux2 = *pos_pfx;
+	for (int i = *pos_pfx; i < (stride_size+aux2); ++i)
 	{
 		if(pfx[i] == 1){
 			aux1 = 2;
 			for (int i = 1; i < aux; ++i) aux1 = aux1*2;
 			pos = pos+aux1;
 		}
-		if (i == (stride_size+pos-1) && pfx[i] == 1) pos--;
+		if (i == (stride_size+aux2-1) && pfx[i] == 1) pos--;
 		aux--;
-		*pos_pfx = *pos_pfx++;
+		*pos_pfx = *pos_pfx + 1;
 	}
 
 	return pos;

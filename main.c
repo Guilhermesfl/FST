9bae1c575aa8d9eb56c2120de2a7efaec4535e4a
Guilhermesfl <gsfl3101@gmail.com>
2017-03-09 15:32:48 -0300

    Insert function tested and working

--- main.c
+++ main.c
@@ -5,22 +5,13 @@
 int main(int argc, char const *argv[])
 {
 	FSTnode *head_node;
-	int stride_size,pos_pfx;
-	int a1[] = {0};
-	int a2[] = {0,0,0,0,1};
-	int a3[] = {0,0,0,1};
-	int a4[] = {0,0,1};
-	int a5[] = {0,0,1,0,0,0,0};
-	int a6[] = {0,0,1,0,0,0,1};
-	int a7[] = {0,0,1,0,1};
-	int a8[] = {0,0,1,1,1};
-	int a9[] = {1,0};
+	int stride_size,pos_pfx=0;
+	int a1[] = {1,1,1,0,1,0,1,1,0};
 
-
-	stride_size = 1;
+	stride_size = 3;
 
 	head_node = NewNode(stride_size);
-	head_node = insert(head_node,a1,stride_size,&pos_pfx,1);
+	head_node = insert(head_node,a1,stride_size,&pos_pfx,9);
 
 	return 0;
 }

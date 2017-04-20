700b6e701aab53581db566a34612d029db64bf06
Guilherme <gsfl3101@gmail.com>
2017-04-20 17:54:43 -0300

    Modified input parameters and added usage function

--- main.c
+++ main.c
@@ -2,19 +2,28 @@
 #include <stdio.h>
 #include "FST.h"
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 	FSTnode *head_node;
-	int stride_size = 2;
-	FILE *pfx, *addr;
+	int stride_size;
+	FILE *pfx24,*pfx32, *addr;
 
-	pfx = fopen(argv[1], "r");
-	addr = fopen(argv[2], "r");
+	if(argc != 5){
+		print_usage(argv);
+		return 0;
+	}
+
+	stride_size = (atoi)(argv[1]);
+	pfx24 = fopen(argv[2], "r");
+	pfx32 = fopen(argv[3], "r");
+	addr = fopen(argv[4], "r");
 	head_node = NewNode(stride_size);
-	read_prefixes(pfx, head_node, stride_size);
+	read_prefixes(pfx24, head_node, stride_size);
+	read_prefixes(pfx32, head_node, stride_size);
 	read_addr(addr,head_node,stride_size);
 
-	fclose(pfx);
+	fclose(pfx24);
+	fclose(pfx32);
 	fclose(addr);
 
 	return 0;

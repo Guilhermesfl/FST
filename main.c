42eb630f06b35d53ad9968924add4dfc214326a0
Guilhermesfl <gsfl3101@gmail.com>
2017-07-04 16:03:11 -0300

    Modified to run with just one prefix base and add free memory function call

--- main.c
+++ main.c
@@ -6,24 +6,22 @@ int main(int argc, char *argv[])
 {
 	FSTnode *head_node;
 	int stride_size;
-	FILE *pfx24,*pfx32, *addr;
+	FILE *pfx, *addr;
 
-	//if(argc != 5){
-	//	print_usage(argv);
-	//	return 0;
-	//}
+	if(argc != 4){
+		print_usage(argv);
+		return 0;
+	}
 
 	stride_size = (atoi)(argv[1]);
-	pfx24 = fopen(argv[2], "r");
-	//pfx32 = fopen(argv[3], "r");
+	pfx = fopen(argv[2], "r");
 	addr = fopen(argv[3], "r");
 	head_node = NewNode(stride_size);
-	read_prefixes(pfx24, head_node, stride_size);
-	//read_prefixes(pfx32, head_node, stride_size);
+	read_prefixes(pfx, head_node, stride_size);
 	read_addr(addr,head_node,stride_size);
 
-	fclose(pfx24);
-	//fclose(pfx32);
+	free_memory(head_node,stride_size);
+	fclose(pfx);
 	fclose(addr);
 
 	return 0;

b7d463fad729feb813f016222f018fc291e0134b
Guilhermesfl <gsfl3101@gmail.com>
2017-07-04 16:02:02 -0300

    Added function to free memory

--- FST.h
+++ FST.h
@@ -5,6 +5,7 @@
 typedef struct fstnode FSTnode;
 typedef struct n_entry entry;
 typedef struct ipv4_prefix ipv4_pfx;
+
 struct fstnode{
 	entry *entries; 
 };
@@ -29,4 +30,5 @@ ipv4_pfx* new_ipv4_prefix(uint8_t a, uint8_t b, uint8_t c, uint8_t d);
 void read_prefixes(FILE *pfxs_file, FSTnode *head_node, int stride_size);
 ipv4_pfx* new_ipv4_addr(uint8_t a, uint8_t b, uint8_t c, uint8_t d, ipv4_pfx *entry);
 void read_addr(FILE *addrs_file, FSTnode *head_node,int stride_size);
+void free_memory(FSTnode* node, int stride_size);
 void print_usage(char *argv[]);
